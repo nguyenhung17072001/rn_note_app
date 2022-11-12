226f21f383797b85e3fe3cd2a37ea32ff71bdc64
Nguyễn Hưng <74828782+nguyenhung17072001@users.noreply.github.com>
2022-11-13 02:25:14 +0700

    update

--- src/Screens/Main/Home/Home.js
+++ src/Screens/Main/Home/Home.js
@@ -1,13 +1,16 @@
 import React, {useEffect, useState} from "react";
-import { View, Text, Image, StyleSheet, SafeAreaView, Dimensions, StatusBar } from "react-native";
+import { View, Text, Image, StyleSheet, SafeAreaView, Dimensions, StatusBar, TouchableOpacity } from "react-native";
 import theme from "../../../Core/theme";
 const {icons, images, colors} = theme;
 const {height, width} = Dimensions.get('window')
 import styles from "./HomeStyle";
+import { Calendar } from "react-native-calendars";
 const Home=()=> {
     
     //event
-
+    const vacation = {key: 'vacation', color: 'red', selectedDotColor: 'blue'};
+    const massage = {key: 'massage', color: 'blue', selectedDotColor: 'blue'};
+    const workout = {key: 'workout', color: 'green'};
 
     return (
         <SafeAreaView style={styles.container}>
@@ -17,8 +20,18 @@ const Home=()=> {
                     <Image  style={styles.avatar} source={images.basicAvatar} resizeMode={"contain"}/>
                     <Text style={{color: 'white', fontSize: 16}}>Nguyễn Hưng</Text>
                 </View>
-                <Image style={styles.addIcon} source={icons.add} />
+                <TouchableOpacity>
+                    <Image style={styles.addIcon} source={icons.add} />
+                </TouchableOpacity>
+                
             </View>
+            <Calendar
+                markingType={'multi-dot'}
+                markedDates={{
+                    '2017-10-25': {dots: [vacation, massage, workout], selected: true, selectedColor: 'red'},
+                    '2017-10-26': {dots: [massage, workout], disabled: true}
+                }}
+            />
 
             
         </SafeAreaView>
